fe8009cf6b31d8a6a9d5ab55df5f0b2b4e4a2a57
Benjamin James <bjames@airmail.cc>
2015-11-15 02:15:58 -0600

    stuff

--- src/ui.py
+++ src/ui.py
@@ -9,6 +9,16 @@ class PollPollUI(QtGui.QWidget):
         def __init__(self):
                 super(PollPollUI, self).__init__()
 
+        def keyPressEvent(self, e):
+                if e.key() == QtCore.Qt.Key_Escape:
+                        self.close()
+
+        def keyword_changed(self, keystr):
+                print("keyword: " + keystr)
+
+        def member_changed(self, memstr):
+                print("member: " + memstr)
+
         def initUI(self, member_string_list, keyword_string_list):
                 member_layout = QtGui.QHBoxLayout()
                 member_description = QtGui.QLabel("Member")
@@ -16,10 +26,12 @@ class PollPollUI(QtGui.QWidget):
 
                 member_model = QtGui.QStringListModel()
                 member_model.setStringList(member_string_list)
-                member_completer = QtGui.QCompleter()
-                member_completer.setModel(member_model)
+                self.member_completer = QtGui.QCompleter()
+                self.member_completer.setCompletionMode(QtGui.QCompleter().InlineCompletion)
+                self.member_completer.setModel(member_model)
+                self.member_completer.highlighted.connect(self.member_changed)
                 member_le = QtGui.QLineEdit(self)
-                member_le.setCompleter(member_completer)
+                member_le.setCompleter(self.member_completer)
                 member_layout.addWidget(member_le)
 
                 keyword_layout = QtGui.QHBoxLayout()
@@ -29,30 +41,31 @@ class PollPollUI(QtGui.QWidget):
                 keyword_model.setStringList(keyword_string_list)
 
                 keyword_completer = QtGui.QCompleter()
+                keyword_completer.activated.connect(self.keyword_changed)
                 keyword_completer.setModel(keyword_model)
 
                 keyword_le = QtGui.QLineEdit(self)
                 keyword_le.setCompleter(keyword_completer)
                 keyword_layout.addWidget(keyword_le)
 
-                table = QtGui.QTableWidget()
+                self.table = QtGui.QTableWidget()
 
-                rows = table.rowCount()
-                columns = table.columnCount()
+                rows = self.table.rowCount()
+                columns = self.table.columnCount()
                 for i in range(rows):
                         for j in range(columns):
-                                item = table.items(i,j)
-                                item.setFlags(QtCore.Qt.ItemIsEditable, false)
+                                item = self.table.items(i,j)
+                                item.setFlags(QtCore.Qt.ItemIsEditable, False)
 
                 #table.setSizePolicy(QtGui.QSizePolicy.Expanding,QtGui.QSizePolicy.Expanding)
-                table.setRowCount(100)
-                table.setColumnCount(4)
-                table.setHorizontalHeaderLabels(["Bill", "Voter", "Passed", "Description"])
+                self.table.setRowCount(100)
+                self.table.setColumnCount(4)
+                self.table.setHorizontalHeaderLabels(["Bill", "Voter", "Passed", "Description"])
 
                 main_layout = QtGui.QVBoxLayout()
                 main_layout.addLayout(member_layout)
                 main_layout.addLayout(keyword_layout)
-                main_layout.addWidget(table)
+                main_layout.addWidget(self.table)
                 self.setLayout(main_layout)
 
                 self.setGeometry(300, 300, 800, 600)
