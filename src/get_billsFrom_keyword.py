58d682c5170a8004ec9902e1c0d2c8fd6c892e04
cdgiessen <cdgiessen@gmail.com>
2015-11-14 19:11:18 -0600

    made function that takes out the irrelevant votes

---
+++ src/get_billsFrom_keyword.py
@@ -0,0 +1,15 @@
+import re
+
+def selectBills(dict,string):
+    #looks through the dict and checks to see if any of them don't have the keyword.
+    if not string:
+        return dict
+    else:
+        newList = {}
+        p = re.compile(string)
+        for value in range(0,len(dict)):
+            if p.search(dict[value][3]) != None:
+                newList[value] = dict[value]
+        return newList
+
+
