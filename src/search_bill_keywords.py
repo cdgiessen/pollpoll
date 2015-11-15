512087366666b53dcc6e8905c60f2916bd0862b1
ahasting <arichasting@gmail.com>
2015-11-14 23:38:44 -0600

    edited names and cleaned up search function

--- src/search_bill_keywords.py
+++ src/search_bill_keywords.py
@@ -1,25 +1,20 @@
 import re
 
-#myDict = {0:["","","","hello"],1:["","","","hello world"],2:["","","","yeahno"]}
-
-#def main():
-#    print(selectBills(myDict,["world","yeahno"]))
-
-def selectBills(dict, stringList):
+def select_bills(dict, stringList):
     #looks through the dict and checks to see if any of them don't have the keyword.
     if len(stringList) == 0:
         return dict
     else:
         newList = {}
+        
         for value in range(0,len(dict)):
             allRelevant = False
+            
             for stringValue in range(0,len(stringList)):
                 p = re.compile(stringList[stringValue])
                 if p.search(dict[value][3]) != None:
                     allRelevant = True
+                    
             if allRelevant:
                 newList[value] = dict[value]
         return newList
-
-#if __name__ == "__main__":
-#    main()
