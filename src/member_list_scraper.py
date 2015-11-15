994f37383cc52727569b953bcb6c91798f58f1c2
ahasting <arichasting@gmail.com>
2015-11-14 21:20:39 -0600

    Print output of all names followed by "top kek"

--- src/member_list_scraper.py
+++ src/member_list_scraper.py
@@ -1,13 +1,13 @@
-from bs4 import BeautifulSoup
 from urllib2 import urlopen
 import re
-
-BASE_URL = "http://projects.washingtonpost.com/"
 
 def get_member_links(section_url):
     html = urlopen(section_url).read()
     
-    regex = re.search('[A-Z][0-9]{6}(?=">).*(?=</a>)', html)
-    return regex.group()
+    regex = re.findall('[A-Z][0-9]{6}(?=">).*(?=</a>)', html)
+    split_list = [ex.split('">') for ex in regex]
+    for st in split_list:
+        print st
+    return "top kek"
 
 print get_member_links("http://projects.washingtonpost.com/congress/113/house/members/")
