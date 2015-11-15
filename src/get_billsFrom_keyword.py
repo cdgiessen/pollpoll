d1e56a9e7f3174c8352a6374113759478359f572
cdgiessen <cdgiessen@gmail.com>
2015-11-14 20:35:58 -0600

    Now will get bills that include any keyword

--- src/get_billsFrom_keyword.py
+++ src/get_billsFrom_keyword.py
@@ -1,14 +1,25 @@
 import re
 
-def selectBills(dict, string = ""):
+#myDict = {0:["","","","hello"],1:["","","","hello world"],2:["","","","yeahno"]}
+
+#def main():
+#    print(selectBills(myDict,["world","yeahno"]))
+
+def selectBills(dict, stringList):
     #looks through the dict and checks to see if any of them don't have the keyword.
-    if string == "":
+    if len(stringList) == 0:
         return dict
     else:
         newList = {}
-        p = re.compile(string)
         for value in range(0,len(dict)):
-            if p.search(dict[value][3]) != None:
+            allRelevant = False
+            for stringValue in range(0,len(stringList)):
+                p = re.compile(stringList[stringValue])
+                if p.search(dict[value][3]) != None:
+                    allRelevant = True
+            if allRelevant:
                 newList[value] = dict[value]
         return newList
 
+#if __name__ == "__main__":
+#    main()
